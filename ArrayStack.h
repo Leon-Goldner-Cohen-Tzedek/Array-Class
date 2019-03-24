4a3eb7fe21770c899a2dd92108da34482e3a0ad2
Leon Katz <lgoldner@ringmountain.org>
2019-03-23 20:28:34 -0700

    I will add docs later lol

---
+++ ArrayStack.h
@@ -0,0 +1,63 @@
+#include "Array.h"
+
+using namespace std;
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+class ArrayStackException : public exception {};
+class ArrayStackOverflow : public ArrayStackException {} StackOverflow;
+class ArrayStackUnderflow : public ArrayStackException {} StackUnderflow;
+
+template <class X>
+class ArrayStack
+{
+private:
+  Array<X>* stack;
+  int head;
+public:
+  void Push(X& value)
+  { //two exceptions UNDERFLOW (too little values ) and OVERFLOW (too many values)
+    try
+    {
+      if (stack->Size() == head) throw StackOverflow;
+      (*stack)[this->head] = value;
+    }
+    catch (ArrayStackOverflow e) {cout << "StackOverflow" << endl;}
+    head++;
+  }
+
+  X Pop()
+  {
+    try
+    {
+      if (head < 1) throw StackUnderflow;
+      head--;
+      return (*stack)[this->head];
+    }
+    catch (ArrayStackUnderflow e) {cout << "StackUnderflow" << endl;}
+
+  }
+
+  X Peek()
+  {
+    try
+    {
+    if (head < 1) throw StackUnderflow;
+    return (*stack)[this->head - 1];
+    }
+    catch (ArrayStackUnderflow e) {cout << "StackUnderflow" << endl;}
+  }
+
+  ArrayStack()
+  {
+    this->stack = new Array<X>(10);
+    this->head = 0;
+  }
+  ArrayStack(int size)
+  {
+    this->stack = new Array<X>(size);
+    this->head = 0;
+  }
+};
+#endif
